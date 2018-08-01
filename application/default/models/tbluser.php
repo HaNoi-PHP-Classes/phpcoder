0b0f8c2aec99e975f45f24d9c47cec15241b1a3a
Tho <thopn.hvcsnd@gmail.com>
2018-08-01 10:30:08 +0700

    save user model

--- application/default/models/tbluser.php
+++ application/default/models/tbluser.php
@@ -1,5 +1,22 @@
 <?php
 class Default_Models_tblUser extends Libs_Model{
     //object properties
+    public $id;
+    public $firstname;
+    public $lastname;
+    public $email;
+    public $contact_number;
+    public $address;
+    public $password;
+    public $access_level;
+    public $access_code;
+    public $status;
+    public $create;
+    public $modified;
+
+    public function __construct(){
+        parent::__construct();
+    }
+
     
 }
